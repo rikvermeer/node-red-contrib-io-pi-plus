2e946c925d8ba9b7e80998f481a30f9094632a04
Rik Vermeer <rik@weare52dn.com>
2019-10-10 17:13:31 +0200

    Added config nodes

--- io-pi-plus.js
+++ io-pi-plus.js
@@ -1,15 +1,42 @@
 const mcp23017 = require('mcp23017');
 
 module.exports = function(RED) {
+
+	function MCP23017ConfigNode(n) {
+        RED.nodes.createNode(this, config);
+
+        this.bus = config.bus;
+        this.address = config.address;
+
+        this.mcp23017 = new mcp23017.IOPi(bus, address);
+    }
+
+    RED.nodes.registerType("mcp23017-config", MCP23017ConfigNode);
+
+    function MCP23017PortConfigNode(n) {
+        RED.nodes.createNode(this, config);
+
+        this.configNode = RED.nodes.getNode(config.mcp23017Config);
+        this.mcp23017 = this.configNode.mcp23017;
+
+        this.bus = this.configNode.bus;
+        this.address = this.configNode.address;
+        this.direction = config.direction;
+		this.init = config.init;
+
+    }
+    RED.nodes.registerType("mcp23017-port-config", MCP23017PortConfigNode);
+
 	function MCP23017WriteNode(config) {
 		RED.nodes.createNode(this, config);
 		
-		this.bus = config.bus;
-		this.address = config.address;
-		this.direction = config.direction;
-		this.init = config.init;
-		
-		this.IOPi = new mcp23017.IOPi();
+		this.portConfigNode = RED.nodes.getNode(config.mcp23017PortConfig);
+		this.mcp23017 = this.portConfigNode.mcp23017;
+
+		this.bus = this.portConfigNode.bus;
+		this.address = this.portConfigNode.address;
+		this.direction = this.portConfigNode.direction;
+		this.init = this.portConfigNode.init;
 
 		var node = this;
 		node.on('input', function(msg) {
