e2dd082e943f5b900b633fe25660bea4190d102d
Rik Vermeer <rik@weare52dn.com>
2019-10-12 22:29:34 +0200

    bugfix

--- io-pi-plus.js
+++ io-pi-plus.js
@@ -26,6 +26,11 @@ module.exports = function(RED) {
         this.direction = config.direction;
 		this.init = config.init;
 
+		//Hardcoded, replace with vars		
+		this.mcp23017.setPortDirection(0, 0);
+		this.mcp23017.setPortDirection(1, 0);
+		this.mcp23017.writePort(0, 0xFF);
+		this.mcp23017.writePort(1, 0xFF);
     }
     RED.nodes.registerType("mcp23017-port-config", MCP23017PortConfigNode);
 
@@ -48,4 +53,42 @@ module.exports = function(RED) {
 		});
 	}
 	RED.nodes.registerType('mcp23017-write-node', MCP23017WriteNode);
+
+	function MCP23017WritePin(config) {
+		RED.nodes.createNode(this, config);
+		
+		this.portConfigNode = RED.nodes.getNode(config.mcp23017PortConfig);
+		this.mcp23017 = this.portConfigNode.mcp23017;
+
+		this.bus = this.portConfigNode.bus;
+		this.address = this.portConfigNode.address;
+		this.direction = this.portConfigNode.direction;
+		this.init = this.portConfigNode.init;
+
+		var node = this;
+		node.on('input', function(msg) {
+			msg.payload = msg.payload;
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType('mcp23017-write-pin', MCP23017WritePin);
+
+	function MCP23017WritePort(config) {
+		RED.nodes.createNode(this, config);
+		
+		this.portConfigNode = RED.nodes.getNode(config.mcp23017PortConfig);
+		this.mcp23017 = this.portConfigNode.mcp23017;
+
+		this.bus = this.portConfigNode.bus;
+		this.address = this.portConfigNode.address;
+		this.direction = this.portConfigNode.direction;
+		this.init = this.portConfigNode.init;
+
+		var node = this;
+		node.on('input', function(msg) {
+			msg.payload = msg.payload;
+			node.send(msg);
+		});
+	}
+	RED.nodes.registerType('mcp23017-write-port', MCP23017WritePort);
 }
